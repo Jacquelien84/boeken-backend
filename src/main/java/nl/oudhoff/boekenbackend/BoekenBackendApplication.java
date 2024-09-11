88cbd9bbd87d60a031077d33455bb19c75174643
Jacquelien84 <jacquelien.oudhoff@outlook.com>
2024-09-11 17:35:31 +0200

    project for books started

---
+++ src/main/java/nl/oudhoff/boekenbackend/BoekenBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.oudhoff.boekenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoekenBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoekenBackendApplication.class, args);
+    }
+
+}
